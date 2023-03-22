3d191343fb8eaa70cf8deb6b73dc577db01e130a
Woffes <squidg4ce@gmail.com>
2023-03-22 09:10:37 +0100

    update 22/3-23

--- sketch.js
+++ sketch.js
@@ -15,17 +15,17 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  background(0);
 
   let x=0
   let y=0
   for (let tile of tiles) {
     print(x,y)
-    imaget(tile.image,x,y,200,200)
-    x+=800/4
-    if(x>800){
-      x=0
-      y+=800/4
+    image(tile.image,x,y,200-5,200-5)
+    y+=800/4
+    if(y>=800){
+      y=0
+      x+=800/4
     }
   }
 }
